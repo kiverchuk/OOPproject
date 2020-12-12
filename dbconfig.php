3832adb19f83e6ae45917d2710a8796569d82776
kiverchuk <32567219+kiverchuk@users.noreply.github.com>
2020-12-12 09:20:38 +0200

    Structura proiectului

---
+++ dbconfig.php
@@ -0,0 +1,8 @@
+<?
+class DBConfig{
+	public static $DBName = "miniproiect";
+	public static $DBUser = "miniuser";
+	public static $DBPass = "MiniUserPass";
+	
+}
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?
+require_once("dbconfig.php");
+require_once("classes.php");
+header('Content-Type: text/html; charset=utf-8');
