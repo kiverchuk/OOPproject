730d42ff7ead428cd820d191fe49b20542522266
kiverchuk <32567219+kiverchuk@users.noreply.github.com>
2020-12-12 09:26:12 +0200

    BD query metode, parser interface + abstract class

--- classes.php
+++ classes.php
@@ -1,6 +1,16 @@
 <?
-class Parser
+class Parser //extends AbstractClass implements Template
 {
 	
+}
+interface Template{
+	public function parsUrl($content);
+	public function popularRegular($url)
+}
+abstract class AbstractClass{
+	private $today;
+	private $lastParser;
+	abstract protected function insertToday();
+	abstract protected function isLastParsToday();
 }
 ?>
--- dbconfig.php
+++ dbconfig.php
@@ -3,6 +3,22 @@ class DBConfig{
 	public static $DBName = "miniproiect";
 	public static $DBUser = "miniuser";
 	public static $DBPass = "MiniUserPass";
-	
+	public static $Conn;
+	public static $ActualArrayFilms;
+	public static function connect(){
+		return DBConfig::$Conn = mysqli_connect("localhost", DBConfig::$DBUser, DBConfig::$DBPass, DBConfig::$DBName);
+	}
+	public static function queryGet($string){
+		$data = DBConfig::$Conn->query($string);
+		$array = array();
+		while( $line = mysqli_fetch_assoc($data) )
+		{
+			$array[$line['idkp']] = $line;
+		}
+		return $array;
+	}
+	public static function querySet($string){
+		return DBConfig::$Conn->query($string);
+	}
 }
 ?>
