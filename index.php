9cce66ebee52a645d0ffe5c136793891607fb232
kiverchuk <32567219+kiverchuk@users.noreply.github.com>
2020-12-12 09:28:47 +0200

    afisarea listei

--- index.php
+++ index.php
@@ -29,3 +29,37 @@ if(!$parser->isLastParsToday()){
 	DBConfig::$ActualArrayFilms = DBConfig::queryGet("SELECT * FROM `toplist` order by num");
 	$parser->insertToday();
 }
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>ТОП 50 популярных</title>
+</head>
+<body>
+	<table border="1">
+		<caption><a href="https://www.kinopoisk.ru/popular/films/" target="_blank">ТОП 50 популярных</a></caption>
+		<tr>
+			<th>№</th>
+			<th>ИДКП</th>
+			<th>Название</th>
+			<th>Дата</th>
+			<th>Рейтинг</th>
+		</tr>
+		<?
+		foreach (DBConfig::$ActualArrayFilms as $value) {
+			$string = "<tr>";
+
+			$string .= "<td>".$value["num"]."</td>";
+			$string .= "<td>".$value["idkp"]."</td>";
+			$string .= "<td>".$value["title"]."</td>";
+			$string .= "<td>".$value["date"]."</td>";
+			$string .= "<td>".$value["rating"]."</td>";
+
+			$string .= "</tr>";
+			echo $string;
+		}
+		?>
+	</table>
+</body>
+</html>
